ec43e36dd635d8d559cf44f84077a9183b37cc0f
Nilesh Bhise <nybhise@gmail.com>
2026-01-04 16:02:23 -0800

    Update AssemblyInfo to include detailed informational version

--- obj/Release/net9.0/learningapp.AssemblyInfo.cs
+++ obj/Release/net9.0/learningapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b4c1df55f0a9c5979b49eebe19a83648ffdeaa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
